4c7012229e4c0211ea26cc9f1f2e5977ef896daa
문진석 <m_jinseok@naver.com>
2020-12-30 21:21:48 +0900

    블록 생성 메소드 정의

--- src/index.ts
+++ src/index.ts
@@ -39,4 +39,26 @@ const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
+const createNewBlock = (data: string): Block => {
+  const prevBlock: Block = getLatestBlock();
+  const newIndex: number = prevBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    prevBlock.hash,
+    data,
+    newTimestamp
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    prevBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock("Hello"), createNewBlock("ByeBye"));
+
 export {};
