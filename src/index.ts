cf0a53b68f9aad0c0b270cbd4a3ab7533bc58adf
문진석 <m_jinseok@naver.com>
2020-12-30 20:36:27 +0900

    클래스 사용해보기

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-interface Human {
-  name: string;
-  age: number;
-  gender?: string;
+class Human {
+  public name: string;
+  public gender: string;
+  public age: number;
+  // private money: number; // 보호
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
-const person: Human = {
-  name: "nicolas",
-  gender: "male",
-  age: 22,
-};
+
+const nico = new Human("nicolas", 22, "male");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(nico));
 
 export {}; // 타입스크립트 룰
