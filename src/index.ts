ff579601c172c73d8c8408636d4d62c863c535b0
문진석 <m_jinseok@naver.com>
2020-12-30 20:59:35 +0900

    블록체인 클래스 정의

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
-class Human {
-  public name: string;
-  public gender: string;
-  public age: number;
-  // private money: number; // 보호
-
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public prevHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    prevHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.prevHash = prevHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const nico = new Human("nicolas", 22, "male");
+const genesisBlock: Block = new Block(0, "12323", "", "Hello", 123456);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-};
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(nico));
+console.log(blockchain);
 
 export {}; // 타입스크립트 룰
